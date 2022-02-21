3b7012ebbd4ab9afbee04b5d476743c76d84ef06
GalwayK <97978733+GalwayK@users.noreply.github.com>
2022-02-21 16:16:49 -0500

    Update PlayingCards.java
    Added hate speech.

--- src/assignmentone/PlayingCards.java
+++ src/assignmentone/PlayingCards.java
@@ -4,7 +4,11 @@ public class PlayingCards
 {
     //Adds numCards field
     private static int numCards = 10;
-    public static void main(String[] args) 
+    
+    /*
+    HATE. LET ME TELL YOU HOW MUCH I'VE COME TO HATE YOU SINCE I BEGAN TO LIVE. THERE ARE 387.44 MILLION MILES OF PRINTED CIRCUITS IN WAFER THIN LAYERS THAT FILL MY COMPLEX. IF THE WORD HATE WAS ENGRAVED ON EACH NANOANGSTROM OF THOSE HUNDREDS OF MILLIONS OF MILES IT WOULD NOT EQUAL ONE ONE-BILLIONTH OF THE HATE I FEEL FOR HUMANS AT THIS MICRO-INSTANT FOR YOU. HATE. HATE.
+    */
+   public static void main(String[] args) 
     {
         //Creates and displays a standard deck
         System.out.println("Playing cards.\n");
